4dcd5930ae4fdfacf7553cee84db5d82d5d6d355
Luiz Fernando <luizepauloxd@gmail.com>
2020-08-27 16:11:16 -0300

    feat: store event emitter

---
+++ src/storeEmitter.ts
@@ -0,0 +1,24 @@
+export type Listener = (storeName: string, next: any) => void;
+
+export default () => {
+  const listeners: Listener[] = [];
+  const initial: Record<string, any> = {};
+
+  return {
+    getInitial: () => initial,
+    setInitial: (storeName: string, next: any): void => {
+      initial[storeName] = next;
+    },
+    subscribe: (listener: Listener): void => {
+      listeners.push(listener);
+    },
+    unsubscribe: (listener: Listener): void => {
+      listeners.splice(listeners.indexOf(listener), 1);
+    },
+    emit: (storeName: string, next: any): void => {
+      for (const listener of listeners) {
+        listener(storeName, next);
+      }
+    },
+  };
+};
